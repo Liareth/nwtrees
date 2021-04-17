393f8256700f461122957a0e6a500c3cbf7b0fe5
Liareth <liarethnwn@gmail.com>
2021-04-17 18:51:33 +0100

    Add debug compiler.

--- src/compiler/main.cpp
+++ src/compiler/main.cpp
@@ -1,6 +1,82 @@
+#include <nwtrees/Assert.hpp>
 #include <nwtrees/Lexer.hpp>
 
-int main(int argc, char** argv)
-{
+#include <chrono>
+#include <filesystem>
 
+std::string read_source_file(const char* path)
+{
+    FILE* file = std::fopen(path, "rb");
+
+    if (file)
+    {
+        std::fseek(file, 0, SEEK_END);
+        std::size_t len = ftell(file);
+        std::fseek(file, 0, SEEK_SET);
+
+        std::string out;
+        out.resize(len);
+
+        size_t read = std::fread(out.data(), 1, len, file);
+        NWTREES_ASSERT(read == len);
+
+        std::fclose(file);
+        return out;
+    }
+
+    return "";
+}
+
+int main(const int argc, const char** argv)
+{
+    std::filesystem::path folder = "D:/_nwn/_server_codebases";
+
+    std::vector<std::filesystem::path> scripts_to_build;
+
+    for (const std::filesystem::directory_entry& entry : std::filesystem::recursive_directory_iterator(folder))
+    {
+        if (std::filesystem::is_regular_file(entry) &&
+            entry.path().extension() == ".nss")
+        {
+            scripts_to_build.push_back(entry.path());
+        }
+    }
+
+    float total_time = 0.0f;
+    float avg_tokens_per_char = 0.0f;
+
+    for (const std::filesystem::path& path : scripts_to_build)
+    {
+        printf("Building %s\n", path.filename().string().c_str());
+
+        printf(" Lexer ");
+        nwtrees::LexerOutput lexer;
+
+        const std::string file = read_source_file(path.string().c_str());
+
+        const auto before = std::chrono::high_resolution_clock::now();
+        lexer = nwtrees::lexer(file.c_str());
+        const auto after = std::chrono::high_resolution_clock::now();
+
+        const float ms = std::chrono::duration_cast<std::chrono::nanoseconds>(after - before).count() / 1000000.0f;
+        total_time += ms;
+
+        const float tokens_per_char = lexer.tokens.empty() ? 0.0f : file.length() / (float)lexer.tokens.size();
+        avg_tokens_per_char += tokens_per_char;
+
+        if (lexer.errors.empty())
+        {
+            printf("SUCCESS (%.2f ms) %zd tokens %.2f tpc\n", ms, lexer.tokens.size(), tokens_per_char);
+        }
+        else
+        {
+            printf("ERROR: %d\n", lexer.errors[0].code);
+            __debugbreak();
+        }
+    }
+
+    avg_tokens_per_char /= scripts_to_build.size();
+
+    printf("Total runtime: %.2f ms, avg tpc: %.2f", total_time, avg_tokens_per_char);
+    fflush(stdout);
 }
